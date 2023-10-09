e2fddfa14b161ab0e4d2eb8da11e79b5c1afd0a3
Najmo Abdalla <najmoabdalla234@gmail.comgit config --global init.defult branch amin gitgit config git config git config --global user.email najmoabdalla234@gmail.com>
2023-10-09 20:16:54 +0100

    function

--- challenge.js
+++ challenge.js
@@ -1,36 +1,96 @@
-for(let i = 0; i < 20; i++) {
-    console.log("Najmo")
-}
+// write a for loop that prints your name 20 times
+
+// for(let i = 0; i < 20; i++) {
+//     console.log("Ahmed")
+// }
+
+
+// write a while that prints your surname 20 times
+// let i = 0;
+// while (i < 20) {
+//     console.log("Dini");
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     console.log("Dini from do while");
+//     i++;
+// } 
+
+// while ( i < 5 )
 
 
 
-let i = 0;
-while (i < 20) {
-    console.log("Abdalla");
-    i++;
-}
+
+// write if/else if, chained code to check what day of the day it is and print something
+
+// let today = "monday";
+
+// if(today == "Monday") {
+//     console.log("It is Coding Day")
+// } else if (today == "Tuesday") {
+//     console.log("It is my off day")
+// } else {
+//     console.log("ask me personally which day")
+// }
+
+// convert the above if/else to switch
+
+// switch (today) {
+//     case "Monday":
+//         console.log("It is Coding Day, from switch")
+//         break;
+//     case "Tuesday":
+//         console.log("It is my off day, from switch")
+//         break;
+
+//     default:
+//         console.log("ask me personally which day, from switch")
+//         break;
+// }
+
+// function multiplyNumbers(num1, num2) {
+//     const result = num1 * num2;
+//     return result;
+//   }
+  
+//   const number1 = 5;
+//   const number2 = 3;
+//   const multiplicationResult = multiplyNumbers(number1, number2);
+  
+//   console.log(`The result of multiplying ${number1} and ${number2} is: ${multiplicationResult}`);
+  
+// function multiplyAndPrint(num1, num2) {
+//     console.log(num1 * num2);
+//   }
+  
+//   multiplyAndPrint(5, 3);
 
 
-let today = "monday";
+// function multiply ( num1, num2){
+// let result = num1 * num2;
+//    return result; 
+//  }
 
-if(today == "Monday") {
-    console.log("It is Coding Day")
-} else if (today == "Tuesday") {
-    console.log("It is my off day")
-} else {
-    console.log("ask me personally which day")
-}
+//  let sum =  multiply (5,6)
+//  console.log(sum)
 
 
-switch (today) {
-    case "Monday":
-        console.log("It is Coding Day, from switch")
-        break;
-    case "Tuesday":
-        console.log("It is my off day, from switch")
-        break;
+ function multiply ( num1, num2) {
+    let result = num1 * num2;
+    return result;
+ }
+ let sum = multiply (5,6)
+console.log(sum)
 
-    default:
-        console.log("ask me personally which day, from switch")
-        break;
+
+function multiply ( num1, num2) 
+return
+
+
+
+function mul (num1, num2) {
+    let result = num1 * num2;
+    return 
 }
